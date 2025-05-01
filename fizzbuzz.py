7218bea07a0486a3aaa1c00d57a4970225caeb98
srikal <srikaldata@gmail.com>
2025-05-01 17:39:47 -0400

    fizzbuzz - attempt 1

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+# fizzbuzz attempt 1
+for num in range(1, 101):
+    if num % 3 == 0:
+        print("Fizz", end="")
+        if num % 5 == 0:
+            print("Buzz", end="")
+    elif num % 5 == 0:
+        print("Buzz", end="")
+    else:
+        print(num, end="")
+    print('')
