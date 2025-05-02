1144bc89b735c5cd4092f09b6ddc38335b0d2648
srikal <srikaldata@gmail.com>
2025-05-01 21:27:45 -0400

    fizzbuzz - submission 2 - using while loop

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,3 +9,17 @@ for num in range(1, 101):
     else:
         print(num, end="")
     print('')
+
+# fizzbuzz submission 2 - using while loop
+num = 1
+while num <= 100:
+    if num % 3 == 0:
+        print("Fizz", end="")
+        if num % 5 == 0:
+            print("Buzz", end="")
+    elif num % 5 == 0:
+        print("Buzz", end="")
+    else:
+        print(num, end="")
+    print('')
+    num+=1
