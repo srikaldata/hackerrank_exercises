f37e1e189d945b10695bdcd213e389c3b39d9d2a
srikal <srikaldata@gmail.com>
2025-05-03 19:42:16 -0400

    003 compare the triplets - submission 1 - using a for loop

---
+++ 003_compare_the_triplets.py
@@ -0,0 +1,29 @@
+# submission 1 - using for loop
+def compareTriplets(a, b):
+    # Write your code here
+    
+    # initializing the points earned 
+    points = [0,0]
+    
+    # loop through each element
+    for i in range(len(a)):
+        
+        # when a element is greater
+        if a[i] > b[i]:
+            points[0] += 1
+            
+        # when b element is greater
+        elif a[i] < b[i]:
+            points[1] += 1
+        
+        # when a and b elements are equal
+        elif a[i] == b[i]:
+            continue
+    
+    return points
+
+
+alice = [10,10,10,12]
+bob = [9,10,11,10]
+
+print(compareTriplets(alice, bob))
