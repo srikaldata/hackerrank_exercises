571eea7daacdda1c729abd9aaea811e89b9abb04
srikal <srikaldata@gmail.com>
2025-05-02 21:35:19 -0400

    002 simple array sum - submission 2 - using for loop

--- 002_simple_array_sum.py
+++ 002_simple_array_sum.py
@@ -1,3 +1,18 @@
 # submission 1 - using in built sum function
-ar = [1,2,3,4,5]
+ar = [1,2,3,4,5, 1]
 print(sum(ar))
+
+
+# submission 2 - using for loop 
+def simpleArraySum(ar):
+    # Write your code here
+    # instantiating the result variable
+    result = 0
+    
+    # adding each element in the list with the result variable
+    for elem in ar:
+        result += elem
+        
+    return result
+
+print(simpleArraySum(ar))
