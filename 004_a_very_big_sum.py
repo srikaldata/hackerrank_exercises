ddb9e62014fd6cee7864143a0a86632fe0f602bf
srikal <srikaldata@gmail.com>
2025-05-05 20:37:51 -0400

    004 very big sum - non pythonic solution using while loop

--- 004_a_very_big_sum.py
+++ 004_a_very_big_sum.py
@@ -5,6 +5,7 @@ def aVeryBigSum(ar):
     # Write your code here
     return sum(ar)
 
+
 # submission 2 - non pythonic using for loop
 def aVeryBigSum(ar):
     # Write your code here
@@ -12,3 +13,19 @@ def aVeryBigSum(ar):
     for num in ar:
         total += num
     return total
+
+
+# submission 3 - non pythonic using while loop
+def aVeryBigSum(ar):
+    # Write your code here
+    total = 0
+    idx = 0
+    while idx < len(ar):
+        total += ar[idx]
+        idx += 1
+    return total
+
+
+example_ls = [1,2,3,4,5]
+
+print(aVeryBigSum(example_ls))
