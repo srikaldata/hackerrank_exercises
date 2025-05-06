243d3caeeb5b456373f587257a04a348171e936c
srikal <srikaldata@gmail.com>
2025-05-05 20:37:18 -0400

    004 very big sum - non pythonic solution using for loop

--- 004_a_very_big_sum.py
+++ 004_a_very_big_sum.py
@@ -4,3 +4,11 @@
 def aVeryBigSum(ar):
     # Write your code here
     return sum(ar)
+
+# submission 2 - non pythonic using for loop
+def aVeryBigSum(ar):
+    # Write your code here
+    total = 0
+    for num in ar:
+        total += num
+    return total
