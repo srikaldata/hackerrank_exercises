c0abbd6a65af861bc2f2eba5b955527bd20c81f1
srikal <srikaldata@gmail.com>
2025-05-06 20:43:46 -0400

    005 python arithmetic operators - using 3 separate print statements

---
+++ 005_python_arithmetic_operators.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+# submission 1 - using 3 print statements
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
