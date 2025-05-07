449fc51d78c5b8ea224601a275b92141ee004042
srikal <srikaldata@gmail.com>
2025-05-07 18:45:58 -0400

    007 python loops - using a for loop

---
+++ 007_python_loops.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-loops/problem
+
+# submission 1 - using a for loop
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i**2)
