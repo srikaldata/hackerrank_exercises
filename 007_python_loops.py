2846fec6cc50e1bf84554316b479dfeb78520d9d
srikal <srikaldata@gmail.com>
2025-05-07 18:49:50 -0400

    007 python loops - using a list comprehension

--- 007_python_loops.py
+++ 007_python_loops.py
@@ -6,3 +6,10 @@ if __name__ == '__main__':
     
     for i in range(n):
         print(i**2)
+
+
+# submission 2 - using a list comprehension
+if __name__ == '__main__':
+    n = int(input())
+    
+    print(*[i**2 for i in range(n)], sep='\n')
