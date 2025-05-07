658ecf00769067a75614bc2bb32039b2e34340be
srikal <srikaldata@gmail.com>
2025-05-07 19:04:50 -0400

    008 python print - using a list comprehension

--- 008_python_print.py
+++ 008_python_print.py
@@ -6,3 +6,10 @@ if __name__ == '__main__':
 
     for num in range(1, n+1):
         print(num, end='')
+
+
+# submission 2 - using a list comprehension
+if __name__ == '__main__':
+    n = int(input())
+
+    print(*[num for num in range(1, n+1)], sep='')
