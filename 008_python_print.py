edb6e3bd9e40afdd1fdb129cc8848e178d747fd9
srikal <srikaldata@gmail.com>
2025-05-07 19:00:46 -0400

    008 python print - using for loop

---
+++ 008_python_print.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-print/problem
+
+# submission 1 - using a for loop
+if __name__ == '__main__':
+    n = int(input())
+
+    for num in range(1, n+1):
+        print(num, end='')
