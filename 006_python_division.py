9a9e1816dd6cefe0d657496fbbd6d22d0af55cd6
srikal <srikaldata@gmail.com>
2025-05-06 20:51:10 -0400

    006 python division - using 2 print statements

---
+++ 006_python_division.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-division/problem
+
+# submission 1 - using 2 print statements
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    print(a/b)
