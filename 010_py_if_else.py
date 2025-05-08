b751ad24a2818d5155c524a1533c9eb6d80f6277
srikal <srikaldata@gmail.com>
2025-05-08 18:43:08 -0400

    010 py if else - using if else and elif without range

--- 010_py_if_else.py
+++ 010_py_if_else.py
@@ -49,6 +49,9 @@ if __name__ == '__main__':
 
 
 # submission 3 - using if else and elif without list fn
+if __name__ == '__main__':
+    n = int(input().strip())
+    
     # odd check
     if n % 2 == 1:
         print('Weird')
@@ -62,6 +65,30 @@ if __name__ == '__main__':
         
         # even and between 6 and 20
         elif n in range(6,21):
+            print('Weird')
+        
+        # even and greater than 20
+        else:
+            print('Not Weird')
+
+
+# submission 4 - using if else and elif without range
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    # odd check
+    if n % 2 == 1:
+        print('Weird')
+    
+    # even check
+    else:
+        
+        # even and between 2 and 5
+        if 2 <= n <= 5:
+            print('Not Weird')
+        
+        # even and between 6 and 20
+        elif 6 <= n <= 20:
             print('Weird')
         
         # even and greater than 20
