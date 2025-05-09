e9c481f394feb314d85fffb19e5ccacc1e1d27df
srikal <srikaldata@gmail.com>
2025-05-09 17:55:38 -0400

    011 write a function leap year - using if else for each condition separately

--- 011_write_a_function_leap_year.py
+++ 011_write_a_function_leap_year.py
@@ -15,3 +15,22 @@ def is_leap(year):
 
     return leap
 
+
+# submission 2 - using if else for each condition separately (more readable)
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    # div by 400
+    if year % 400 == 0:
+        leap = True
+        
+    # div by 100
+    elif year % 100 == 0:
+        leap = False
+    
+    # div by 4
+    elif year % 4 == 0:
+        leap = True
+
+    return leap
