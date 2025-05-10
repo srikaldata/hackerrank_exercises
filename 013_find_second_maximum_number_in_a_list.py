a798fffab96f44dd1282908875298b14b83652bf
srikal <srikaldata@gmail.com>
2025-05-10 19:38:26 -0400

    013 find second max num in a list - non pythonic loop based solution

--- 013_find_second_maximum_number_in_a_list.py
+++ 013_find_second_maximum_number_in_a_list.py
@@ -13,4 +13,28 @@ if __name__ == '__main__':
     
     # apply the max fn again to find the second max
     print(max(set_arr))
+
+
+# submission 2 - non pythonic solution 
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
     
+    # initializing first max and second max
+    first_max = second_max = float('-inf')
+     
+    # looping through the array
+    for num in arr:
+        
+        # when the current num in the loop is first max
+        if num > first_max:
+            second_max = first_max
+            first_max = num
+        
+        # when the current num in loop is second max
+        elif first_max > num > second_max:
+            second_max = num
+    
+    # print the second max 
+    # (edge case if there is no second max is not handled in this solution)
+    print(second_max)
