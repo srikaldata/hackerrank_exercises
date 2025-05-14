dad3a76c274644aeb9790167350c389141830985
srikal <srikaldata@gmail.com>
2025-05-13 22:28:41 -0400

    017 python string split and join - using replace string method

--- 017_python_string_split_and_join.py
+++ 017_python_string_split_and_join.py
@@ -11,3 +11,14 @@ if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
+
+
+# submission 2 - using replace string method
+def split_and_join(line):
+    # write your code here
+    return line.replace(' ', '-')
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
