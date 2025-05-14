3c4e560332b974bcbc959334822ce282d2079419
srikal <srikaldata@gmail.com>
2025-05-14 15:28:52 -0400

    018 whats your name - using print statement and string manipulation

---
+++ 018_whats_your_name.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+
+# submission 1 - using a print statement and string manipulation
+def print_full_name(first, last):
+    # Write your code here
+    print('Hello', first, last+'!', 'You just delved into python.')
+    
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
