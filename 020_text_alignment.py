3a36803db00689525a5407fb26ab7a2b7c7b2381
srikal <srikaldata@gmail.com>
2025-05-15 15:47:43 -0400

    020 text alignment - using the hints given - top cone

---
+++ 020_text_alignment.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/text-alignment/problem
+
+
+# submission 1 - using the hints given
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
