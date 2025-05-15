4ba7a831c4b13209278941086ae5b0463fd9cc6b
srikal <srikaldata@gmail.com>
2025-05-15 15:50:10 -0400

    020 text alignment - using the hints given - bottom cone

--- 020_text_alignment.py
+++ 020_text_alignment.py
@@ -22,3 +22,7 @@ for i in range((thickness+1)//2):
 #Bottom Pillars
 for i in range(thickness+1):
     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6)) 
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
