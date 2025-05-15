b8127909c6060e8ed8d7b2ab95d54ea9f7bbb9ea
srikal <srikaldata@gmail.com>
2025-05-15 15:49:09 -0400

    020 text alignment - using the hints given - middle belt

--- 020_text_alignment.py
+++ 020_text_alignment.py
@@ -14,3 +14,7 @@ for i in range(thickness):
 #Top Pillars
 for i in range(thickness+1):
     print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
