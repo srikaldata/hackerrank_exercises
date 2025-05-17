1a2c59e19c55eec5414b91dc622074fcbe74eb76
srikal <srikaldata@gmail.com>
2025-05-17 18:25:36 -0400

    022 designer door mat - using separate for loops - getting the dimensions

---
+++ 022_designer_door_mat.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+
+# submission 1 - using separate for loops for each portion of the mat
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# the row by column dimensions
+N, M = map(int, input().split())
