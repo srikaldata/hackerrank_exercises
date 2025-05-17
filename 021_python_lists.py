2fbfbb87ffa1e0cf0f6a681bed1f4e536ebdce27
srikal <srikaldata@gmail.com>
2025-05-16 20:45:36 -0400

    021 python lists - creating an empty list and looping through the number of commands

---
+++ 021_python_lists.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/python-lists/problem
+
+
+# submission 1 - classifying the command and performing the corresponding operations
+if __name__ == '__main__':
+    N = int(input())
+    temp_list = []
+    
+    # looping through the number of commands
+    for _ in range(N):
+            
