91a524128065094aa49652f12123ed501d7cbcaa
srikal <srikaldata@gmail.com>
2025-05-18 17:05:07 -0400

    023 find a string - create a counter and substring length varibles

---
+++ 023_find_a_string.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+
+# submission 1 - pythonic version using a counter and for loop
+def count_substring(string, sub_string):
+    
+    # counter
+    count = 0
+    
+    # substring length
+    sub_string_len = len(sub_string)
