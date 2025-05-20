ff7cea8dafd749022357fa80a843661c9bbe08e6
srikal <srikaldata@gmail.com>
2025-05-20 18:13:23 -0400

    024 string validators - check if the string has any alphanumeric chars

---
+++ 024_string_validators.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/string-validators/problem
+
+# submission 1 - using print, any and string methods 
+if __name__ == '__main__':
+    s = input()
+    
+    # check if the string has any alphanumeric chars
+    print(any(str.isalnum(char) for char in s))
+    
+    # check if the string has any alphabetical chars
+    print(any(str.isalpha(char) for char in s))
+    
+    # check if the string has any digit chars
+    print(any(str.isdigit(char) for char in s))
+    
+    # check if the string has any lowercase chars
+    print(any(str.islower(char) for char in s))
+    
+    # check if the string has any uppercase chars
+    print(any(str.isupper(char) for char in s))
