ae7f678b492f80cac8168857ae67bf6be4d66f4b
srikal <srikaldata@gmail.com>
2025-05-21 13:26:29 -0400

    025 python tuples - get the number of inputs count

---
+++ 025_python_tuples.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+
+# submission 1 - in pypy3 - using hash and tuple nested functions
+if __name__ == '__main__':
+
+    # get the number of inputs added
+    n = int(input())
