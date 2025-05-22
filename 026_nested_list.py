fbd7ff306ba7aeab74ec91f0386c35d7dcf1e823
srikal <srikaldata@gmail.com>
2025-05-22 18:21:27 -0400

    025 nested list - create an empty list

---
+++ 026_nested_list.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/nested-list/problem
+
+
+# submission 1 - pythonic solution by sorting and getting all names with second lowest score
+if __name__ == '__main__':
+    
+    # empty list
+    students = []
+
