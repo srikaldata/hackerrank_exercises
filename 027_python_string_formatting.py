b60c9bc5407892637a01680622c200894217c2a2
srikal <srikaldata@gmail.com>
2025-05-23 19:20:55 -0400

    027 python string formatting - generating the decimal value and placing on the left of text format

--- 027_python_string_formatting.py
+++ 027_python_string_formatting.py
@@ -1,14 +1,14 @@
 # https://www.hackerrank.com/challenges/python-string-formatting/problem
 
 
-# submission 1 - using str oct hex bin fns and text formatting
+# submission 1 - using str(for decimal) oct hex bin fns and text formatting
 def print_formatted(number):
     # your code goes here
     
     # find the width for the space between the values
     width = len(bin(number)[2:])
     
-    return width
-    
-print(print_formatted(30000))
-    
+    # for each number generate all 4 values
+    for i in range(1, number+1):
+        
+        dec = str(i).rjust(width)
